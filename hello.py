1c8a25c3a7330008bd089372aa8b82194ee6e6e1
Robert Clooney <robert.clooney@lseg.com>
2024-11-20 10:35:57 +0000

    add dummy

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
+username = "robert.clooney@lseg.com"
+secret = "aasd-bsf-testy-mctest"
+db = "https://non-existent.lseg.com"
 def main():
+    print(db, secret, username)
     print("Hello from foo!")
-
 
 if __name__ == "__main__":
     main()
